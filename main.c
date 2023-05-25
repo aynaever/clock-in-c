9a09d631e92ec0a3bc1b435b67659663952c4a8c
Aymane <aymane.naouadir18@gmail.com>
2023-05-25 14:07:09 +0100

    printing the month

--- main.c
+++ main.c
@@ -4,12 +4,14 @@
 
 int	main(void)
 {
-	struct timeval	tp;
-	struct tm	*readable_time;
-	char* days[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+	struct	timeval	tp;
+	struct	tm	*readable_time;
+	char*	days[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+	char*	months[12] = {"Jan", "Feb", "Mar", "Apr", "May", 
+				"Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
 	gettimeofday(&tp, NULL);
 	readable_time = localtime(&tp.tv_sec);
-	printf("%s\n", days[readable_time->tm_wday]);
+	printf("%s %s\n", days[readable_time->tm_wday], months[readable_time->tm_mon]);
 	return (0);
 }
