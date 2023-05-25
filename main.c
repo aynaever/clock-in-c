6fd8eda8bedb3a46d6bbb00902d5c4ee198af826
Aymane <aymane.naouadir18@gmail.com>
2023-05-25 14:37:49 +0100

    printing PM or AM, here it's identical to default output of date program

--- main.c
+++ main.c
@@ -6,19 +6,22 @@ int	main(void)
 {
 	struct	timeval	tp;
 	struct	tm	*readable_time;
+	int	hour;
 	char*	days[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 	char*	months[12] = {"Jan", "Feb", "Mar", "Apr", "May", 
 				"Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
 	gettimeofday(&tp, NULL);
 	readable_time = localtime(&tp.tv_sec);
-	printf("%s %s %d %d:%d:%d +%02ld %d\n",
+	hour = (readable_time->tm_hour > 12 ? readable_time->tm_hour - 12 : readable_time->tm_hour);
+	printf("%s %s %d %02d:%d:%d %s +%02ld %d\n",
 			days[readable_time->tm_wday],
 			months[readable_time->tm_mon],
 			readable_time->tm_mday,
-			readable_time->tm_hour,
+			hour,
 			readable_time->tm_min,
 			readable_time->tm_sec,
+			(readable_time->tm_hour > 12 ? "PM" : "AM"),
 			(readable_time->tm_gmtoff / 60 / 60),
 			(readable_time->tm_year + 1900));
 	return (0);
