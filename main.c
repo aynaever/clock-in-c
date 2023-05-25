152f8dc78521165fa0c316478ad386f936d4afb7
Aymane <aymane.naouadir18@gmail.com>
2023-05-25 12:17:20 +0100

    using gettimeofday and timeval struct

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include	<stdio.h>
+#include	<sys/time.h>
 
 int	main(void)
 {
-	printf("Hello, World!");
+	struct	timeval	*timespec;
+
+	gettimeofday(timespec, NULL);
+	printf("tv_sec->%d, suseconds_t->%d",
+			(timespec->tv_sec / 60 / 60 / 24 / 365),
+			timespec->tv_usec / 1000 / 60);
 	return (0);
 }
